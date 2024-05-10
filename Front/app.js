57317ad6199b89a2e0162aca860304fdd381d97b
FcoMdz <fcodmendez24@gmail.com>
2024-05-09 19:36:19 -0600

    cambio de rutas para intentar solucionar lo de electron

---
+++ Front/app.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, // Permite que la ventana principal acceda a Node.js
+      allowFileAccess: true // Permite acceso a los archivos locales
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'dist', 'app-root', 'index.html'));
+
+  // Abre las DevTools solo en modo de desarrollo
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
